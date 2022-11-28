5b092644cb27a7db1ab719f75e9c6c9940aa2730
Emre <esrn@outlook.de>
2022-11-28 22:07:16 +0100

    upgrades people, upgrades

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <CLI/CLI.hpp>
  
-int main ( int argc, char  * argv [ ] ) 
+int main ( int argc, char  ** argv) 
 { 
-    return 0;
+    CLI::App app{"Dies ist ein Programm was uns den Umgang mit Lagerhaltungsprogrammen zeigen soll";"Lagerhaltungsprogramm"}
+    
+    try{
+        app.parse(argc, argv);
+    } catch(const CLI::ParseError& e){
+        return app.exit(e);
+    }
 }
